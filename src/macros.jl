d5ced46af758122aaa715fec9676ffe46774dfe7
Simon Christ <SimonChrist@gmx.de>
2024-02-02 12:39:46 +0100

    create tempdir

--- src/macros.jl
+++ src/macros.jl
@@ -1,6 +1,6 @@
 macro visualtest(testfun, refname, popup=true, tol=1e-3)
   esc(quote
-    testFilename = tempname()*".png"
+    testFilename = joinpath($path, tempname() * ".png")
     $testfun(testFilename)
     @test test_images(testFilename, $refname, popup=$popup, tol=$tol) |> success
   end)
@@ -8,7 +8,7 @@ end
 
 macro plottest(plotfun, refname, popup=true, tol=1e-3)
   esc(quote
-    testFilename = tempname()*".png"
+    testFilename = joinpath($path, tempname() * ".png")
     if $plotfun isa Function
       $plotfun()
     else # function body
